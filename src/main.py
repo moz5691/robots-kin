98e8471ae376dfc85ee289c2d08166a6552af8a9
Chanho Ahn <moz5691@gmail.com>
2022-02-15 23:01:52 -0500

    google oauth2

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import logging
 from fastapi import FastAPI
-from src.api import health, robots
+from src.api import health, robots, auth
 from src.db import init_db
+from starlette.middleware.sessions import SessionMiddleware
 
 
 log = logging.getLogger("uvicorn")
@@ -18,9 +19,13 @@ def create_application() -> FastAPI:
         contact={
             "name": "Chan Ho Ahn",
             "email": "moz5691@gmail.com",
-        }
+        },
+        docs_url=None,
+        redoc_url=None,
     )
-    application.include_router(health.router, prefix="", tags=["Health"])
+    application.add_middleware(SessionMiddleware, secret_key="!secret!")
+    application.include_router(health.router, prefix="/health", tags=["Health"])
+    application.include_router(auth.router, prefix="", tags=["Auth"])
     application.include_router(robots.router, prefix="/robots", tags=["Robots"])
 
     return application
